5e3bc9833d54bc6a3e2cd8809690f2af1cd01880
Ignatius Christanto <christantoignatius@gmail.com>
2022-03-27 21:11:00 +0700

    refactor code into using loop for the data

--- src/Component/song/index.js
+++ src/Component/song/index.js
@@ -1,4 +1,5 @@
-const SongComponent = ({url, name, artists}) =>{
+import './index.css';
+const SongComponent = ({url, name, artists, children}) =>{
     return (
         <div className="song-wrapper">
             <div className="image-wrapper">
@@ -6,12 +7,11 @@ const SongComponent = ({url, name, artists}) =>{
             </div>
             <div className='text-wrapper'>
             <h2 className='song-title'>{name}</h2>
-            <h3 className='song-artist'>{artists}</h3>
+            <h4 className='song-artist'>{artists}</h4>
             </div>
+            {children}
         </div>
     )
-    
-    
 }
 
 export default SongComponent;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import data from '../../sample/single-sample.js';
+import data from '../../sample/data';
 import SongComponent from '../../Component/song';
 import ButtonComponent from '../../Component/button';
 import '../../App.css';
@@ -6,12 +6,19 @@ import '../../App.css';
 const HomePage = () => {
     return (
         <div className="container">
-            <SongComponent
-                url ={data.album.images[1].url}
-                name = {data.name}
-                artists = {data.artists[0].name}
-            />
-            <ButtonComponent />
+            <h1 className='playlist-title'>Your Playlist</h1>
+            {data.map((song,index) =>(
+                <SongComponent
+                    key = {song.id}
+                    url ={song.album.images[1].url}
+                    name = {song.name}
+                    artists = {song.artists[0].name}
+                >
+                    <ButtonComponent />
+                </SongComponent>
+            ))}
+            
+            
         </div>
     )
 }
